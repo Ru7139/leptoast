60a11a045512bb180eee9fb34527a7dd91c15fb5
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-07-30 22:50:35 +0800

    use set when change a string value

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,19 @@ fn App() -> Element {
 #[component]
 fn CounterBlock() -> Element {
     let mut count = use_signal(|| 0usize);
+    let mut message = use_signal(|| "Goooooood".to_string());
+    let mut number = use_signal(|| 725usize);
 
     rsx!(
         h1 { "High-Five counter: {count}" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
+
+        h1 { "message: {message}" }
+        button { onclick: move |_| message.set("Good".to_string()), "Good" }
+        button { onclick: move |_| message.set("Bad".to_string()), "Bad" }
+
+        h1 { "number: {number}" }
+        button { onclick: move |_| number += 5, "Next" }
     )
 }
