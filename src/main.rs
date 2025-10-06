42ea020b46caf5f9c6808db488ed930cbae71f90
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-06 22:12:41 +0900

    separate components

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,53 @@
-mod db;
+#[cfg(feature = "web")]
+mod app {
+    pub mod app_itself;
+}
 
-use dioxus::prelude::*;
-use serde::{Deserialize, Serialize};
+#[cfg(feature = "server")]
+mod backend {
+    pub mod axum_app;
+}
+
+#[cfg(feature = "web")]
+mod router {
+    pub mod homepage;
+    pub mod router_itself;
+}
+
+#[cfg(feature = "server")]
+mod sdb {
+    pub mod common;
+    pub mod read;
+    pub mod write;
+}
+
+// use dioxus::prelude::*;
 
 fn main() {
-    dioxus::launch(App);
+    #[cfg(feature = "server")]
+    tokio::runtime::Runtime::new().unwrap().block_on(async {
+        // sdb::common::sdb_ws_connect_as_ruut(65535).await.unwrap();
+        launch_server()
+    });
+
+    #[cfg(feature = "web")]
+    dioxus::launch(app::app_itself::App);
 }
 
-#[component]
-fn App() -> Element {
-    rsx! {
-        h2 { "Try This Test" }
-    }
-}
+#[cfg(feature = "server")]
+async fn launch_server() {
+    // Connect to dioxus' logging infrastructure
+    dioxus::logger::initialize_default();
 
-#[derive(PartialEq, Debug, Deserialize, Serialize)]
-struct TargetLocation {
-    x: f64,
-    y: f64,
-    z: f64,
-    location_name: String,
+    // Connect to the IP and PORT env vars passed by the Dioxus CLI (or your dockerfile)
+    let socket_addr = dioxus::cli_config::fullstack_address_or_localhost();
+
+    // Build a custom axum router
+    let router = axum::Router::new()
+        // .serve_dioxus_application(ServeConfig::new().unwrap(), App)
+        .into_make_service();
+
+    // And launch it!
+    let listener = tokio::net::TcpListener::bind(socket_addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
