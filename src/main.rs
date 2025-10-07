a38fb1f1579127022402725b52516fa9e2c6058a
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-07 14:10:11 +0900

    hard to use

--- src/main.rs
+++ src/main.rs
@@ -21,33 +21,18 @@ mod sdb {
     pub mod write;
 }
 
-// use dioxus::prelude::*;
+use dioxus::prelude::*;
 
 fn main() {
+    #[cfg(feature = "web")]
+    dioxus::launch(app::app_itself::App);
+
+
+
     #[cfg(feature = "server")]
     tokio::runtime::Runtime::new().unwrap().block_on(async {
         // sdb::common::sdb_ws_connect_as_ruut(65535).await.unwrap();
-        launch_server()
+        //
+        use dioxus::server::
     });
-
-    #[cfg(feature = "web")]
-    dioxus::launch(app::app_itself::App);
-}
-
-#[cfg(feature = "server")]
-async fn launch_server() {
-    // Connect to dioxus' logging infrastructure
-    dioxus::logger::initialize_default();
-
-    // Connect to the IP and PORT env vars passed by the Dioxus CLI (or your dockerfile)
-    let socket_addr = dioxus::cli_config::fullstack_address_or_localhost();
-
-    // Build a custom axum router
-    let router = axum::Router::new()
-        // .serve_dioxus_application(ServeConfig::new().unwrap(), App)
-        .into_make_service();
-
-    // And launch it!
-    let listener = tokio::net::TcpListener::bind(socket_addr).await.unwrap();
-    axum::serve(listener, router).await.unwrap();
 }
