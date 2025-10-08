07d94e8bfb5e7fcf31ba6009be57179d6bcb7106
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-08 12:37:49 +0900

    signal func with default prop

---
+++ src/simple_on_page_item/x003_signal_props.rs
@@ -0,0 +1,46 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn ProgressBar_Default_100(
+    #[prop(default = 100)] max_value: u32,
+    signal_value: ReadSignal<u32>,
+) -> impl IntoView {
+    view! {
+            <progress
+                max = max_value
+                value = move || signal_value.get() // 需要添加move让leptos知道这是一个动态值
+                style="width:320px; height:16px;" // default width:160px, height:16px
+            />
+    }
+}
+
+#[component]
+pub fn ProgressBar_Generic_Default_100<T>(
+    #[prop(default = 100)] max_value: u32,
+    signal_value: T,
+) -> impl IntoView
+where
+    T: Fn() -> u32 + Send + Sync + 'static,
+{
+    view! {
+            <progress
+                max = max_value
+                value = signal_value
+                style="width:320px; height:16px;"
+            />
+    }
+}
+
+#[component]
+pub fn ProgressBar_Into_D100(
+    #[prop(default = 100)] max_value: u32,
+    #[prop(into)] signal_value: Signal<u32>,
+) -> impl IntoView {
+    view! {
+            <progress
+                max = max_value
+                value = signal_value // 需要添加move让leptos知道这是一个动态值
+                style="width:320px; height:16px;" // default width:160px, height:16px
+            />
+    }
+}
