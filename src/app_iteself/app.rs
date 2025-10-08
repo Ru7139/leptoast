3014983756ec1ec794c14ea0544b6fdf5bc294a2
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-08 17:42:48 +0900

    add func

--- src/app_iteself/app.rs
+++ src/app_iteself/app.rs
@@ -1,15 +1,20 @@
 use leptos::prelude::*;
 
 use super::super::simple_on_page_item::{
-    x000_aisatsu::Hello, x001_counter::TheCouter, x002_dynamic_attributes::ShowDifferentColor,
-    x004_iteration::Vec_View_Basic,
+    x000_aisatsu::Hello,
+    x001_counter::TheCouter,
+    x002_dynamic_attributes::ShowDifferentColor,
+    x004_iteration::{Forview, VecViewBasic},
+    x005_iterating_over_complex_data::ComplexDataIter,
 };
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
         <div>
-            <Vec_View_Basic times=3/>
+            <ComplexDataIter/>
+            <VecViewBasic times=3/>
+            <Forview/>
             <ShowDifferentColor/>
             <TheCouter initial_value=0 step_value=1/>
             <Hello/>
