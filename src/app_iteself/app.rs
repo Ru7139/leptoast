86d133ddcee439285beef72c384d2df1a2658fd2
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-09 11:57:45 +0900

    so far so good

--- src/app_iteself/app.rs
+++ src/app_iteself/app.rs
@@ -5,13 +5,17 @@ use super::super::simple_on_page_item::{
     x001_counter::TheCouter,
     x002_dynamic_attributes::ShowDifferentColor,
     x004_iteration::{Forview, VecViewBasic},
-    x005_iterating_over_complex_data::{BetterIterA, ComplexDataIter},
+    x005_iterating_over_complex_data::{
+        BetterIterA, BetterIterMemo, ComplexDataIter, MaybeBestIterStores,
+    },
 };
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
         <div>
+            <MaybeBestIterStores/>
+            <BetterIterMemo/>
             <BetterIterA/>
             <ComplexDataIter/>
             <VecViewBasic times=3/>
