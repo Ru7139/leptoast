d89d8fd95c918d4326e4414149935f76143e781d
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-09 22:51:33 +0900

    <label>

--- src/simple_on_page_item/x006_forms_and_inputs.rs
+++ src/simple_on_page_item/x006_forms_and_inputs.rs
@@ -15,3 +15,54 @@ pub fn ControlledInputsV0() -> impl IntoView {
     }
 }
 
+#[component]
+pub fn ControlledInputsV1() -> impl IntoView {
+    let (rx, tx) = signal("Cooperation 2".to_string());
+    let email = RwSignal::new("".to_string());
+    let favorite_color = RwSignal::new("red".to_string());
+    let spam_me = RwSignal::new(true);
+
+    view! {
+        <h3> "---> ControlledInputsV1()" </h3>
+        <p>
+            "Name: "
+            <input type = "text" bind:value = (rx,tx) />
+        </p>
+
+        <p>
+            "Email: "
+            <input type = "email" bind:value = email />
+            <br> </br>
+            <label>
+                " Please send me lots of spam email"
+                <input type = "checkbox" bind:value = spam_me/>
+            </label>
+        </p>
+
+
+
+        <fieldset>
+            <legend> "Favorite color" </legend>
+            <label>
+                "Red"
+                <input type="radio" name="color" value="Red" bind:group = favorite_color/>
+            </label>
+            <label>
+                "Green"
+                <input type="radio" name="color" value="Green" bind:group = favorite_color/>
+            </label>
+            <label>
+                "Blue"
+                <input type="radio" name="color" value="Blue" bind:group = favorite_color/>
+            </label>
+        </fieldset>
+
+        <p> "Your favorite color is: " {favorite_color} </p>
+        <p> "Name is: " {rx} </p>
+        <p> "Email is: " {email} </p>
+        <Show when = move || spam_me.get()>
+            <p> "You'll receive coll bonus content!" </p>
+        </Show>
+    }
+}
+
