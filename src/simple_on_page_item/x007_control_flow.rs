32a4c1f6f5bb68d60f725a5a17c19dec6ea64943
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-11 16:23:51 +0900

    <Show> </Show>
    类似于when，并且不会反复渲染

--- src/simple_on_page_item/x007_control_flow.rs
+++ src/simple_on_page_item/x007_control_flow.rs
@@ -8,5 +8,25 @@ pub fn IfControl() -> impl IntoView {
     view! {
         <h3> "---> IfControl()" </h3>
 
+pub fn BetterUseShow() -> impl IntoView {
+    let (rx_data, tx_data) = signal(0u32);
+    fn DataIsBig() -> impl IntoView { view! {<p> "Big" </p>} }
+    fn DataIsSmall() -> impl IntoView { view! {<p> "Small" </p>} }
+
+    view! {
+        <h3> "---> BetterUseShow()" </h3>
+        <Show // 只会渲染1次，不会反复渲染
+            when = move || { rx_data.get() <= 5 }
+            fallback = || view! { <DataIsBig/> }
+        >
+            <DataIsSmall/> // 包括这里也只会渲染1次
+        </Show>
+
+        <span> {rx_data} " " </span>
+
+        <button on:click = move |_|
+                tx_data.update(|x| *x += 1)>
+            " +1 button"
+        </button>
     }
 }
