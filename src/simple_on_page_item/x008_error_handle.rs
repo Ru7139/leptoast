595548743e19e14268f8474e74642f313346d281
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-13 19:07:46 +0900

    ErrorBoundary need to be review

--- src/simple_on_page_item/x008_error_handle.rs
+++ src/simple_on_page_item/x008_error_handle.rs
@@ -23,7 +23,39 @@ pub fn NumericInput() -> impl IntoView {
 
 #[component]
 pub fn ErrorBoundary() -> impl IntoView {
-    let (rx_data, tx_data) = signal(0i32);
+    let (rx_data, tx_data) = signal(Ok(0i32));
+    let _ = rx_data;
 
-    view! {}
+    view! {
+        <div>
+            <h3> "---> ErrorBoundary()" </h3>
+            <label>
+                "Type a number(or something is not a number)"
+                <br/>
+                <input type = "number" on:input:target = move |user_input| {
+                   tx_data.set(user_input.target().value().parse::<i32>());
+                }/> // 如果使用Some<T>的方式，输入字母也是无效的，和上面的相同
+
+                // Something wrong with this part
+                // <ErrorBoundary
+                //     fallback = |errors| view! {
+                //         <div class = "error">
+                //             <p> "Not a number! Errors: " </p>
+                //             <ul>
+                //                 {
+                //                     move || errors.get()
+                //                                 .into_iter()
+                //                                 .map(|(_,e)| view! { <li> {e.to_string()} </li> })
+                //                                 .collect::<Vec<_>>()
+                //                 }
+                //             </ul>
+                //         </div>
+                //     }
+                // >
+                //     <p> "You entered: " <strong> {rx_data} </strong> </p>
+                // </ErrorBoundry>
+
+            </label>
+        </div>
+    }
 }
