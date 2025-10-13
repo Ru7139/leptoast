5911e309f0ca4db6db90577a6c197a5a6ad09950
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-13 19:04:14 +0900

    Update app.rs

--- src/app_iteself/app.rs
+++ src/app_iteself/app.rs
@@ -12,13 +12,18 @@ use super::super::simple_on_page_item::{
         ControlledInputsV0, ControlledInputsV1, ControlledInputsV2, SelectSpecial, TextareaSpecial,
     },
     x007_control_flow::{BetterUseShow, IfControl},
-    x008_error_handle::NumericInput,
+    x008_error_handle::{ErrorBoundary, NumericInput},
+    x009_parentChild_communication::PassTXSignalPlusOne,
 };
 
 #[component]
 pub fn App() -> impl IntoView {
+    let (rx_data, tx_data) = signal(0u64);
+
     view! {
         <div>
+            <PassTXSignalPlusOne rx_signal=rx_data tx_signal=tx_data/>
+            <ErrorBoundary/>
             <NumericInput/>
             <BetterUseShow/>
             <IfControl/>
