85686a9db3fb598771c81118f86492f1d0067ecf
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-23 21:56:08 +0900

    simple html parts 1

--- src/simple_on_page_item/x000_aisatsu.rs
+++ src/simple_on_page_item/x000_aisatsu.rs
@@ -1,4 +1,7 @@
-use leptos::prelude::*;
+use leptos::{
+    html::{b, div, h1, h6, i, li, ol, p, table, th, tr, ul},
+    prelude::*,
+};
 
 #[component]
 pub fn Hello() -> impl IntoView {
@@ -8,4 +11,54 @@ pub fn Hello() -> impl IntoView {
             <h3> "おハヨウございます☀️leptosより" </h3>
         </div>
     }
+}
+
+#[component]
+pub fn HtmlExercise() -> impl IntoView {
+    div().child((
+        h1().child("Using h1"),
+        h6().child("using h6"),
+        p().child(("段落标签", " ", b().child("加粗"), " ", i().child("斜体"))),
+        ul().child((
+            li().child("无序列表元素1"),
+            li().child("无序列表元素2"),
+            li().child("无序列表元素3"),
+        )),
+        ol().child((
+            li().child("有序列表元素1"),
+            li().child("有序列表元素2"),
+            li().child("有序列表元素3"),
+        )),
+        table()
+            .attr(
+                "style",
+                "border-collapse: collapse; border: 1px solid black;",
+            )
+            .child((
+                tr().child((
+                    th().attr("style", "border: 1px solid black;")
+                        .child("列标题1"),
+                    th().attr("style", "border: 1px solid black;")
+                        .child("列标题2"),
+                    th().attr("style", "border: 1px solid black;")
+                        .child("列标题3"),
+                )),
+                tr().child((
+                    th().attr("style", "border: 1px solid black;")
+                        .child("elem_x 1"),
+                    th().attr("style", "border: 1px solid black;")
+                        .child("elem_x 2"),
+                    th().attr("style", "border: 1px solid black;")
+                        .child("elem_x 3"),
+                )),
+                tr().child((
+                    th().attr("style", "border: 1px solid black;")
+                        .child("elem_y 1"),
+                    th().attr("style", "border: 1px solid black;")
+                        .child("elem_y 2"),
+                    th().attr("style", "border: 1px solid black;")
+                        .child("elem_y 3"),
+                )),
+            )),
+    ))
 }
