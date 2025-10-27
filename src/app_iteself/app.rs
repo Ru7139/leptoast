2e04c204945894baecdf6a380acdf8f41480f857
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-27 20:44:43 +0900

    move into async

--- src/app_iteself/app.rs
+++ src/app_iteself/app.rs
@@ -1,24 +1,28 @@
 use leptos::prelude::*;
 
-use super::super::simple_on_page_item::{
-    x000_aisatsu::{Hello, HtmlExercise},
-    x001_counter::TheCouter,
-    x002_dynamic_attributes::ShowDifferentColor,
-    x004_iteration::{Forview, VecViewBasic},
-    x005_iterating_over_complex_data::{
-        BetterIterA, BetterIterMemo, ComplexDataIter, MaybeBestIterStores,
+use super::super::{
+    s0_simple_on_page_item::{
+        x000_aisatsu::{Hello, HtmlExercise},
+        x001_counter::TheCouter,
+        x002_dynamic_attributes::ShowDifferentColor,
+        x004_iteration::{Forview, VecViewBasic},
+        x005_iterating_over_complex_data::{
+            BetterIterA, BetterIterMemo, ComplexDataIter, MaybeBestIterStores,
+        },
+        x006_forms_and_inputs::{
+            ControlledInputsV0, ControlledInputsV1, ControlledInputsV2, SelectSpecial,
+            TextareaSpecial,
+        },
+        x007_control_flow::{BetterUseShow, IfControl},
+        x008_error_handle::{ErrorBoundary, NumericInput},
+        x009_parentchild_communication::{DeeplyNestedComponent, PassTXSignalPlusOne},
+        x010_passing_children_to_components::{ComponentChildren, HiddenMessage, WrapsChildren},
+        x011_the_view_builder_syntax::NoViewAtensionButtonI32,
+        x012_working_with_signal::UseTheSignal,
+        x013_responding_to_changes_with_effects::{UseEffect, UseEffectWatch},
+        x014_reactivity_and_functions::InterludeAndFunctions,
     },
-    x006_forms_and_inputs::{
-        ControlledInputsV0, ControlledInputsV1, ControlledInputsV2, SelectSpecial, TextareaSpecial,
-    },
-    x007_control_flow::{BetterUseShow, IfControl},
-    x008_error_handle::{ErrorBoundary, NumericInput},
-    x009_parentchild_communication::{DeeplyNestedComponent, PassTXSignalPlusOne},
-    x010_passing_children_to_components::{ComponentChildren, HiddenMessage, WrapsChildren},
-    x011_the_view_builder_syntax::NoViewAtensionButtonI32,
-    x012_working_with_signal::UseTheSignal,
-    x013_responding_to_changes_with_effects::{UseEffect, UseEffectWatch},
-    x014_reactivity_and_functions::InterludeAndFunctions,
+    s1_async::{x0_loading_data_with_resource::UseResource, x1_suspense::UseSuspense},
 };
 
 #[component]
@@ -27,6 +31,8 @@ pub fn App() -> impl IntoView {
 
     view! {
         <div>
+            <UseSuspense/>
+            <UseResource/>
             <InterludeAndFunctions/>
             <UseEffectWatch/>
             <UseEffect/>
