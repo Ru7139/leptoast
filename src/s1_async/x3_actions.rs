668ba07a9804c16dc485fafc6d8a0d8202917fa3
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-10-31 23:02:05 +0900

    Update x3_actions.rs

--- src/s1_async/x3_actions.rs
+++ src/s1_async/x3_actions.rs
@@ -1,1 +1,10 @@
+use gloo_timers::future::TimeoutFuture;
+use leptos::prelude::*;
 
+#[component]
+pub fn UseActions() -> impl IntoView {}
+
+async fn add_todo_request(new_title: &str) -> String {
+    TimeoutFuture::new(800).await;
+    return "server side is OK".to_string();
+}
