ae1268adcac083459f87c66be1fa022e6f9146da
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-11-01 22:10:53 +0900

    done action code

--- src/s1_async/x3_actions.rs
+++ src/s1_async/x3_actions.rs
@@ -1,10 +1,53 @@
 use gloo_timers::future::TimeoutFuture;
-use leptos::prelude::*;
+
+use leptos::{
+    ev,
+    html::{Input, br, button, div, form, h3, input, label, p},
+    prelude::*,
+};
 
 #[component]
-pub fn UseActions() -> impl IntoView {}
+pub fn UseActions() -> impl IntoView {
+    let action1 = Action::new(|input: &String| {
+        let i = input.to_owned();
+        async move { add_todo_request(&i).await }
+    });
 
-async fn add_todo_request(new_title: &str) -> String {
-    TimeoutFuture::new(800).await;
-    return "server side is OK".to_string();
+    let submitted = action1.input();
+    let pending = action1.pending();
+    let todo_id = action1.value();
+
+    // Action接受一个async函数，这个函数接受对单个参数的引用
+    // let _action2 = Action::new(|_input: &String| async { todo!() });
+    // let _action3 = Action::new(|_input: &(usize, String)| async { todo!() });
+
+    let input_ref = NodeRef::<Input>::new();
+
+    div().attr("class", "UseActions").child((
+        h3().child("---> UseActions()"),
+        form()
+            .on(ev::submit, move |evs| {
+                evs.prevent_default();
+                let input = input_ref.get().expect("input to exist");
+                action1.dispatch(input.value());
+            })
+            .child(label().child((
+                p().child("What do you need to do?"),
+                input().attr("type", "text").node_ref(input_ref),
+                br(),
+                button().attr("type", "submit").child("Add Todo"),
+            ))),
+        p().child(("Pending:", move || pending.get().then_some("Loading..."))),
+        p().child(("Submit: ", move || format!("{:#?}", submitted.get()))),
+        p().child(("Pending: ", move || format!("{:#?}", pending.get()))),
+        p().child(("Todo ID: ", move || format!("{:#?}", todo_id.get()))),
+    ))
+}
+
+async fn add_todo_request(new_title: &str) -> Result<String, ServerFnError> {
+    // TimeoutFuture::new(800).await;
+    // future cannot be sent between threads safely
+    // the trait Send is not implemented for (dyn FnMut() + 'static)
+    send_wrapper::SendWrapper::new(TimeoutFuture::new(800)).await;
+    return Ok(format!("server side is OK with {}", new_title));
 }
